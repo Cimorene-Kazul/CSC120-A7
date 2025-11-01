29476640241c04d34ac131736526ee1b8a922c22
Cimorene-Kazul <eliana.lippa@gmail.com>
2025-11-01 17:02:11 -0400

    uncommented code  with no/few print statements

--- House.java
+++ House.java
@@ -1,12 +1,42 @@
 /* This is a stub for the House class */
-public class House {
 
-  public House() {
+import java.util.ArrayList;
+
+public class House extends Building implements HouseRequirements{
+  // additional attributes
+  private ArrayList<Student> residents; // The <Student> tells Java what kind of data we plan to store IN the ArrayList
+  private boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+    super(name, address, nFloors);
+    this.residents = new ArrayList<Student>();
+    this.hasDiningRoom = hasDiningRoom;
     System.out.println("You have built a house: 🏠");
   }
 
+  public boolean hasDiningRoom(){
+    return this.hasDiningRoom;
+  }
+
+  public int nResidents(){
+    return this.residents.size();
+  }
+
+  public void moveIn(Student s){
+    this.residents.add(s);
+  }
+
+  public Student moveOut(Student s){
+    this.residents.remove(s);
+    return s;
+  }
+
+  public boolean isResident(Student s){
+    return residents.contains(s);
+  }
+
   public static void main(String[] args) {
-    new House();
+    new House("Tyler", "102 Tyler Avenue", 5, true);
   }
 
 }
--- Library.java
+++ Library.java
@@ -1,12 +1,79 @@
 /* This is a stub for the Library class */
-public class Library {
 
-    public Library() {
+import java.util.Hashtable;
+
+public class Library extends Building implements LibraryRequirements{
+    private Hashtable<String, Boolean> collection;
+
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      this.collection = new Hashtable<String, Boolean>();
       System.out.println("You have built a library: 📖");
     }
-  
+
+    public boolean containsTitle(String title){
+      return this.collection.containsKey(title);
+    }
+    public boolean isAvailable(String title){
+      return this.collection.getOrDefault(title, false);
+    }
+    public void printCollection(){
+      System.out.println("============================================");
+      System.out.println("PRINTED LIST OF "+this.name+" COLLECTION");
+      for (String key : collection.keySet()){
+        if (this.collection.get(key)){
+          System.out.println(key+" - AVALIABLE");
+        } else{
+          System.out.println(key+" - CHECKED OUT");
+        }
+      }
+      System.out.println("============================================");
+    }
+
+    public void addTitle(String title){
+      if (!this.containsTitle(title)){
+        this.collection.put(title, true);
+      } else {
+        System.out.println("The book "+title+" is already in the collection and thus cannot be added.");
+      }
+    }
+
+    public String removeTitle(String title){
+      if (this.containsTitle(title)){
+        this.collection.remove(title);
+      } else {
+        System.out.println("The book "+title+" is not in the collection and thus cannot be removed.");
+      }
+      return title;
+    }
+
+    public void checkOut(String title){
+      if (this.isAvailable(title)){
+        this.collection.put(title, false);
+      } else {
+        System.out.println("The book "+title+" is unavailiable and thus cannot be checked out.");
+      }
+    }
+
+    public void returnBook(String title){
+      if (!this.isAvailable(title)){
+        this.collection.put(title, true);
+      } else {
+        System.out.println("The book title "+title+" is still in the collection and thus cannot be returned.");
+      }
+    }
+
     public static void main(String[] args) {
-      new Library();
+      System.out.println("test");
+      Library frostLibrary = new Library("Robert Frost Library", "12 Mammoth Street", 6);
+      frostLibrary.addTitle("The Lorax by Dr. Seuss");
+      frostLibrary.printCollection();
+      frostLibrary.checkOut("The Lorax by Dr. Seuss");
+      frostLibrary.printCollection();
+      frostLibrary.returnBook("The Lorax by Dr. Seuss");
+      frostLibrary.printCollection();
+      frostLibrary.removeTitle("The Lorax by Dr. Seuss");
+      frostLibrary.printCollection();
     }
   
   }
